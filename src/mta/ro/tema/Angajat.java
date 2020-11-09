cdc1d5a95bc080948c612eeea86431e4736c0e22
Cristotovici <cristian.ghita3@yahoo.com>
2020-11-09 19:23:57 +0200

    Adaugare clasa angajat
    Signed-off-by: Cristotovici <cristian.ghita3@yahoo.com>

---
+++ src/mta/ro/tema/Angajat.java
@@ -0,0 +1,21 @@
+package mta.ro.tema;
+
+public class Angajat {
+
+
+    private String nume;
+    private double salariu;
+    private int an_angajare;
+
+
+    Angajat(String nume,double salariu, int an_angajare){
+        this.nume=nume;
+        this.salariu=salariu;
+        this.an_angajare=an_angajare;
+    }
+    @Override
+    public String toString() {
+        return "Angajatul cu numele "+ this.nume+", angajat in anul "+this.an_angajare+" are salariul de "+this.salariu;
+    }
+
+}
